1d2d2c5ce3efe296c5a49d79afe6fa22aeb0a847
Nina Potrebich <potrebich@gmail.com>
2020-02-28 16:30:43 -0800

    Add toggling showing of Child info

--- src/components/Child.jsx
+++ src/components/Child.jsx
@@ -6,17 +6,34 @@ class Child extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            points: 0
+            points: 0,
+            showInfo: false
         }
     }
+
+    handleShowInfoClick = () => {
+        this.setState({showInfo: !this.state.showInfo});
+    }
+
     render() {
         let yearDifference = Moment(new Date()).diff(Moment(this.props.birthday), 'years');
+        const name = this.state.showInfo ? "Hide Info" : "Show Info";
         return (
             <div>
-                <h2>Child information</h2>
-                <p>Name: {this.props.name}</p>
-                <p>Birthday: {this.props.birthday}</p>
-                <p>Your child is {yearDifference} years old.</p>
+                <h3>{this.props.name}</h3>
+                <button type="button" className="btn btn-primary" onClick={this.handleShowInfoClick}>{name}</button>
+                {this.state.showInfo ? 
+                    <div>
+                        <h2>Child information</h2>
+                        <p>Name: {this.props.name}</p>
+                        <p>Birthday: {this.props.birthday}</p>
+                        <p>Your child is {yearDifference} years old.</p>
+                        <p>Points: {this.state.points}</p>
+                        <button type="button" className="btn btn-success">Edit Child</button>
+                        <button type="button" className="btn btn-danger">Delete Child</button>
+                    </div>
+                    : null
+                }
             </div>
         );
     }
