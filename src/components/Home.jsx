c7257624ec87dd1db1d9c8f6a31072ab62ff53b6
Nina Potrebich <potrebich@gmail.com>
2020-03-03 16:09:35 -0800

    Add text to the home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,14 +6,20 @@ import { FirebaseContext } from './Firebase';
 
 const Home = (props) => {
     return (
-        <div>
-            Home
-            <FirebaseContext.Consumer>
+        <div class="container home">
+            {/* <FirebaseContext.Consumer>
                 {firebase => {
                     console.log(firebase);
                     return <div>I've access to Firebase and render smthing:</div>
                 }}
-            </FirebaseContext.Consumer>
+            </FirebaseContext.Consumer> */}
+            <br/>
+            <h1>
+                Welcome to Kids Points Tracker!
+            </h1>
+            <p>This is application that helps you to track points earned by children. You can assign different categories, have multiple items inside your categories and assign different amount of points for doing this items. You can track points for multiple children. You can continue track your points even during vacation. At least if you access to Internet!</p>
+            <p>Don't know where to start?</p>
+            <p>You can register your account or just start playing without saving data.</p>
 
             <div class="sea">
                 <div class="circle-wrapper">
