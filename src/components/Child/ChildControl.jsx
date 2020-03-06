b49b166c8e076cecb6daf59e905559353ebff34c
Nina Potrebich <potrebich@gmail.com>
2020-03-06 11:56:17 -0800

    Add Redeem Points Form. Update state. Not connected to DB

--- src/components/Child/ChildControl.jsx
+++ src/components/Child/ChildControl.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import {Redirect} from 'react-router-dom';
-import { Button } from 'react-bootstrap';
+import { Button, Form } from 'react-bootstrap';
+import Select from 'react-select';
 
 class ChildControl extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            name: "", birthday: null, points: 0, history: []
+            name: "", 
+            birthday: null, 
+            points: 0, 
+            history: [], 
+            categories: {}, 
+            showCategories: false, 
+            showItems: false,
+            showQuantity: false,
+            showRedeemButton: false,
+            showHistory: false
         }
+        this._category = null;
+        this._item = null;
+        this._quantity = 0;
     }
 
     componentDidMount() {
@@ -22,20 +35,109 @@ class ChildControl extends React.Component {
                             history: snapshot.val().history ? snapshot.val().history : []
                         });
                 })
+            .then(() => {
+                this.props.firebase.dbRef.ref("categories/" + this.props.firebase.auth.currentUser.uid + "/").once("value")
+                    .then(snapshot => {
+                        let categories = {};
+                        for (let key in snapshot.val()) {
+                            categories[snapshot.val()[key].name] = [];
+                            for (let itemKey in snapshot.val()[key].items) {
+                                categories[snapshot.val()[key].name].push(snapshot.val()[key].items[itemKey]);
+                            }
+                        }
+                        this.setState({categories: categories})
+                    })
+            })
         }
+    }
+
+    handleRedeemPoints = event => {
+        event.preventDefault();
+        let history = {...this.state.history, [new Date()]: this._category.value + " / " + this._item.value.name + " / " + this._quantity.value + " / " + this._item.value.points};
+        this.setState({
+            points: this.state.points + parseInt(this._item.value.points) * parseInt(this._quantity.value), 
+            history: history, 
+            showCategories: false, 
+            showItems: false,
+            showQuantity: false,
+            showRedeemButton: false
+        })
+    }
+
+    handleRedeemPointsButtonClick = () => {
+        !this.state.showCategories ? 
+            this.setState({
+                showCategories: true, 
+                showItems: false, 
+                showQuanity: false, 
+                showRedeemButton: false})
+                :
+            this.setState({
+                showCategories: false, 
+                showItems: false,
+                showQuantity: false,
+                showRedeemButton: false})
     }
 
     render(){
         if (!this.props.auth) {
             return <Redirect to="/" />
         }
+        const redeemButtonName = this.state.showCategories ? "Hide redeem points form" : "Redeem points";
+
+        let categoriesOptions = [];
+        for (let key in this.state.categories) {
+            categoriesOptions.push({value: key, label: key})
+        }
+
+        let itemsCategories = [];
+        if (this._category) {     
+            for (let i in this.state.categories[this._category.value]) {
+                const item = this.state.categories[this._category.value][i];
+                itemsCategories.push({value: item, label: item.name + " (" + item.points + ")" })
+            }
+        }
+
         return (
             <div className="child">
                 <h1>{this.state.name}</h1>
                 <em>{this.state.birthday}</em>
                 <h3>Number of available points: {this.state.points}</h3>
                 <hr/>
-                <Button varint="info" type="button">Redeem points</Button>
+                <Button variant="info" type="button" onClick={this.handleRedeemPointsButtonClick}>{redeemButtonName}</Button>
+                <hr />
+                <Form onSubmit={this.handleRedeemPoints}>
+                {this.state.showCategories ? 
+                        <Select 
+                            options={categoriesOptions} 
+                            className="select" 
+                            placeholder="Select Category ..." 
+                            onChange={(input) => {this.setState({showItems: true}); this._category = input;}} 
+                            />
+                : null }
+                {this.state.showItems ? 
+                    <Select 
+                        options={itemsCategories} 
+                        className="select" 
+                        placeholder="Select Item ..." 
+                        onChange={(input) => {this.setState({showQuantity: true}); this._item = input}} 
+                        />
+                : null}
+                {this.state.showQuantity ? 
+                    <Form.Group>
+                        <Form.Label className="control-label">Quantity:</Form.Label>
+                        <Form.Control 
+                            type="number" 
+                            name="quantity" 
+                            className="select"
+                            onChange={(input) => {this.setState({showRedeemButton: true}); this._quantity = input}}
+                            ref={(input) => this._quantity = input}/>
+                    </Form.Group>
+                : null}
+                {this.state.showRedeemButton ? 
+                    <Button type="info" type="submit">Redeem</Button>
+                :null}
+                </Form>
             </div>
         );
     }
