701a471f1309ec172f7588edc96d0e4164689227
Nina Potrebich <potrebich@gmail.com>
2020-03-09 15:25:35 -0700

    Delete console.log

--- src/components/Category/CategoryRoute.jsx
+++ src/components/Category/CategoryRoute.jsx
@@ -4,7 +4,6 @@ import {useParams} from 'react-router-dom';
 
 const CategoryRoute = (props) => {
     let {id} = useParams();
-    console.log(props.location.state);
     return <Category {...props} id={id} name={props.location.state.name} items={props.location.state.items}/>
 }
 
